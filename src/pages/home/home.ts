87cb81b1eb3f056e60a756cf9c862c6a00fa2b15
Karl <karlanuetzel@gmail.com>
2017-03-25 03:12:03 -0500

    adding camera functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,26 @@
 import {Component} from '@angular/core';
+import {Camera} from 'ionic-native';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public base64Image: string;
+
   constructor() {
+  }
+
+  takePicture() {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base65," + imageData;
+    }, (err) => {
+      console.log(err);
+
+    });
   }
 }

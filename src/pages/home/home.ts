d892a730af5c570f1a109b595f39069735325fb6
Karl <karlanuetzel@gmail.com>
2017-03-25 11:24:10 -0500

    AJAX

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {Camera, CameraOptions} from '@ionic-native/camera';
+import {Http} from "@angular/http";
+import { Headers, RequestOptions } from '@angular/http';
+
+declare var $: any;
 
 @Component({
   selector: 'page-home',
@@ -9,12 +13,12 @@ import {Camera, CameraOptions} from '@ionic-native/camera';
 export class HomePage {
   public base64Image: string;
 
-  constructor(private camera: Camera) {
+  constructor(private camera: Camera, private http: Http) {
   }
 
   takePicture() {
     const options: CameraOptions = {
-      quality: 100,
+      quality: 30,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
@@ -23,12 +27,40 @@ export class HomePage {
     this.camera.getPicture(options).then((imageData) => {
       this.base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
+      console.log("Image error")
     });
+
   }
 
-  sendPicture(base64Image){
+  sendPicture() {
+
+    /*
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
 
 
+    this.http.post('http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload', {"imageData": "swag"}, options);
+    */
+
+
+    $.ajax({
+
+      type: "POST",
+      url : "http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload",
+      headers: {
+        'Content-Type':'application/jsonp'
+      },
+      data : {"imageData": "hello this is alex"},
+      success: function(data, textStatus, jqXHR)
+      {
+        //data - response from server
+        alert("this worked decent");
+      },
+      error: function (jqXHR, textStatus, errorThrown)
+      {
+        alert("this did work well");
+      }
+    });
 
 
   }

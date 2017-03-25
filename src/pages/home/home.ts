069a90295ee4f837c018a36885532d46802a0aa1
Justin <justinreusnow@gmail.com>
2017-03-25 11:44:36 -0500

    This is barely working.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 import {Camera, CameraOptions} from '@ionic-native/camera';
 import {Http} from "@angular/http";
-import { Headers, RequestOptions } from '@angular/http';
+import {Headers, RequestOptions} from '@angular/http';
+import {Response} from '@angular/http'
+import 'rxjs/add/operator/map';
 
 declare var $: any;
 
@@ -22,47 +24,54 @@ export class HomePage {
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
-    }
+    };
 
     this.camera.getPicture(options).then((imageData) => {
       this.base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
       console.log("Image error")
     });
-
   }
 
   sendPicture() {
-
-    /*
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-
-
-    this.http.post('http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload', {"imageData": "swag"}, options);
-    */
-
-
-    $.ajax({
-
-      type: "POST",
-      url : "http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload",
-      headers: {
-        'Content-Type':'application/jsonp'
-      },
-      data : {"imageData": "hello this is alex"},
-      success: function(data, textStatus, jqXHR)
+    let url = 'http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload';
+    let body =
       {
-        //data - response from server
-        alert("this worked decent");
-      },
-      error: function (jqXHR, textStatus, errorThrown)
-      {
-        alert("this did work well");
-      }
-    });
+        "imageData": "swag"
+      };
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
+    this.http
+      .post(url, body, options)
+      .map(
+        (response: Response) => {
+          console.log(response);
+          return response.json();
+        }
+      )
+      .subscribe(
+        (responseBody: Object) => {
+          console.log("Response Body: \"" + responseBody + "\"");
+          return;
+        }
+      );
 
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "http://ec2-34-204-93-190.compute-1.amazonaws.com:3000/upload",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data: {
+    //       "imageData": "asdfasdfsdfasdfasdf"
+    //     },
+    //     success: function (data, textStatus, jqXHR) {
+    //       alert("this worked decent");
+    //     },
+    //     error: function (jqXHR, textStatus, errorThrown) {
+    //       alert("this did work well");
+    //     }
+    //   });
   }
-
 }
